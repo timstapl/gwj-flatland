ea5e6edb35aa1702e88cb8c498d2a2fa83058392
Timothy Stapleton <retrocede@outlook.com>
2020-05-09 19:13:24 -0700

    init commit, player and first character scenes

---
+++ Player/Player.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+export var speed : int = 200
+
+var target = Vector2()
+var velocity = Vector2()
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
+		target = event.position
+
+func _physics_process(_delta):
+	velocity = position.direction_to(target) * speed
+	if position.distance_to(target) > 5:
+		velocity = move_and_slide(velocity)
